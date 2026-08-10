d0cea7ff4448f5cbdcc757f2db5a80cf69638ce7
Riquelme Eduardo <rickprevita@gmail.com>
2026-08-10 10:13:00 -0300

    feat: Estrutura inicial da ferramenta OpsTrack API

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def status():
+    return {
+        "message": "sua criação, esta vivo!"
+    }
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "ok"
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
